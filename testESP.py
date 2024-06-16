11b2ca251bd9436b397d8ca90ecdf42e699206f2
102210292githug <95340358+102210292githug@users.noreply.github.com>
2024-06-16 11:37:04 +0700

    new
    đá

--- testESP.py
+++ testESP.py
@@ -15,7 +15,7 @@ model = load_model("./Other Files/finetuned_vgg_face_model.h5")
 labels = np.load('./Other Files/class_labels.npy', allow_pickle=True).item()
 labels = {v: k for k, v in labels.items()}  # Reverse the dictionary
 face_cascade = cv2.CascadeClassifier('./Other Files/haarcascade_frontalface_default.xml')
-CONFIDENCE_THRESHOLD = 0.8
+CONFIDENCE_THRESHOLD = 0.75
 
 app = Flask(__name__)
 latest_image_lock = threading.Lock()
@@ -27,30 +27,15 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 
-@app.route('/command', methods=['POST'])
-def handle_command():
-    data = request.get_json()  # Lấy dữ liệu JSON từ request
-    command = data.get('command')
-    
-    if command == 'unlock':
-        # Thực hiện lệnh mở khóa
-        print("Unlock command received.")
-        # Thêm code để mở khóa ở đây
-        return "Unlocking", 200
-    elif command == 'lock': 
-        # Thực hiện lệnh khóa cửa
-        print("Lock command received.")
-        # Thêm code để khóa cửa ở đây
-        return "Locking", 200
-    else:
-        return "Command not recognized", 400
 
 last_label = "NoFace"
+from datetime import datetime
+import os
+
 @app.route('/image', methods=['POST'])
 def handle_image():
     global latest_image
     global last_label
-    tmp_image = latest_image
     label = "No Face"
     with latest_image_lock:
         latest_image = request.data
@@ -74,108 +59,64 @@ def handle_image():
             prediction = model.predict(face)
             class_idx = np.argmax(prediction, axis=1)[0]
             confidence = np.max(prediction)
-
             
             if confidence >= CONFIDENCE_THRESHOLD:
                 label = labels[class_idx]
             elif CONFIDENCE_THRESHOLD > 0.1:
                 label = "Unknown"
-    
-    # Gửi ảnh và nhãn đến một server khác trước khi t rả về response
-    # url = "http://192.139.66.104:1713/customers"
-    # files = {'image': ('filename.jpg', latest_image, 'image/jpeg')}
-    # data = {'name': label}
-    # response = requests.post(url, files=files, data=data)
-    
     print(label) 
+    value = 200
+    if label == "Unknown":
+        value += 1
+    if label == "No Face":
+        value += 2
+
     if label != "No Face" and last_label != label:
-        url = "http://192.139.66.104:1713/customers/haveFace"
+        urlApp = "http://192.168.133.124:1713/customers/haveFace"
+        urlEsp = "http://192.168.133.211/" + str(value)
         files = {'image': ('filename.jpg', latest_image, 'image/jpeg')}
         data = {'name': label}
-        response = requests.post(url, files=files, data=data)
-        print(response)
+        # response =
+        requests.post(urlApp, files=files, data=data)
+        requests.get(urlEsp)
+        # print(response)
     last_label = label
     
     
-    # print(label) 
-    latest_image = tmp_image
-    return str(label), 200 + int(label != "Unknown" and label != "No Face")
+
+    return str(label), value
 
 
 
-def display_video():
-    cv2.namedWindow("Live Video Stream")
-    
-    while not should_exit.is_set():
-        with latest_image_lock:
-            if latest_image is not None:
-                nparr = np.frombuffer(latest_image, np.uint8)
-                frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-                # Detect faces
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                    face = frame[y:y+h, x:x+w]
-                    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                    face = cv2.resize(face, (224, 224))
-                    face = img_to_array(face)
-                    face = np.expand_dims(face, axis=0)
-                    face /= 255.0
 
-                    # Predict using the VGG-Face model
-                    prediction = model.predict(face)
-                    class_idx = np.argmax(prediction, axis=1)[0]
-                    confidence = np.max(prediction)
-
-                    if confidence < CONFIDENCE_THRESHOLD:
-                        label = "Unknown"
-                        similar_label = labels[class_idx]
-                        cv2.putText(frame, f'{label}, but looks like {similar_label}: {confidence:.2f}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-                    else:
-                        label = labels[class_idx]
-                        cv2.putText(frame, f'{label}: {confidence:.2f}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-
-                cv2.imshow('Live Video Stream', frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    should_exit.set()
-                    break
-        time.sleep(0.01)
-    cv2.destroyAllWindows()
-    sys.exit()
-
-@app.route('/shutdown', methods=['GET'])
+@app.route('/shutdown', methods=['POST'])
 def shutdown():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
+    shutdown_func = request.environ.get('werkzeug.server.shutdown')
+    if shutdown_func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+    shutdown_func()
     return 'Server shutting down...'
 
 def start_flask_app():
     app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
 
-def shutdown_server():
-    requests.get('http://127.0.0.1:5000/shutdown')  # Trigger Flask server shutdown
+
 
 if __name__ == '__main__':
     # Start Flask in a separate thread
     flask_thread = Thread(target=start_flask_app)
     flask_thread.start()
 
-    # Start the display in a separate thread
-    # video_thread = Thread(target=display_video)
-    # video_thread.start()
-
-
     try:
         while not should_exit.is_set():
-            time.sleep(0.5)  # Reduce CPU usage
+            if cv2.waitKey(500) & 0xFF == ord('q'):
+                print("Exiting the program...")
+                requests.post('http://localhost:5000/shutdown')  # Gửi yêu cầu để dừng Flask
+                should_exit.set()  # Signal to exit the program
+            time.sleep(0.5)
 
-        shutdown_server()  # Shutdown the Flask server
     finally:
         flask_thread.join()
-        # video_thread.join()
-         
+        cv2.destroyAllWindows()  # Make sure to destroy all the cv2 windows
